6479df64ddbaaf8ed50969736efca022eb8fcfdb
Lamija Koca <klamija99@gmail.com>
2021-12-17 20:41:45 +0100

    transfer tokens and delegated transfer

--- contracts/LightToken.sol
+++ contracts/LightToken.sol
@@ -2,15 +2,58 @@
 pragma solidity ^0.8.4;
 
 contract LightToken {
-    //constructor 
-    //set the total number of tokens
-    //read the total number of tokens
+    string public name = "LightToken";
+    string public symbol = "LT";
+    string public standard = "LightToken v1.0";
     uint256 public totalSupply; //how many token exists
-    mapping(address => uint256) public balanceOf;
 
-    function Light(uint256 initialSupply) public{
+    mapping(address => uint256) public balanceOf; 
+    mapping(address => mapping(address => uint256)) public allowance; //state variable
+    //mapping inner mapping -> 1st mapping for 1 acc, 2nd mapping for 2 acc -> to approve uint256 value to be spended or whatever
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+
+    constructor(uint256 initialSupply){
         balanceOf[msg.sender] = initialSupply;
         totalSupply = initialSupply;
     }
     
+    function transfer(address _to, uint256 _value) public returns (bool success){
+        //check balance
+        require(balanceOf[msg.sender] >= _value);
+
+        balanceOf[msg.sender] -= _value;
+        balanceOf[_to] += _value;
+
+        emit Transfer(msg.sender, _to, _value);
+        return true;
+    }
+
+    //if i am account A i want to approve account B to spend amount 
+    function approve(address _spender, uint256 _value) public returns(bool success){
+        allowance[msg.sender][_spender] = _value;
+
+        emit Approval(msg.sender, _spender, _value);
+        
+        return true;
+    }
+
+    //function for a withdraw workflow, allowing contracts to transfer tokens 
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+        //da _from ima dovoljno tokena 
+        require(_value <= balanceOf[_from]);
+        //da je allowance dovoljno velik
+        require(_value <= allowance[_from][msg.sender]);
+        //change balance
+        balanceOf[_from] -= _value;
+        balanceOf[_to] += _value;
+        //update allowance
+        allowance[_from][msg.sender] -= _value;
+
+        return true;
+    }
+    // function getBalance(address adr ) public view returns(uint){
+    //     return balanceOf[adr];
+    // }
 }
--- test/LightToken.js
+++ test/LightToken.js
@@ -2,6 +2,21 @@ var LightToken = artifacts.require("./LightToken.sol");
 
 contract('LightToken', function(accounts){
     var tokenInstance;
+    it('initializes the contract with the correct values', function(){
+        return LightToken.deployed()
+        .then(function(instance){
+            tokenInstance = instance;
+            return tokenInstance.name();
+        }).then(function(name){
+            assert.equal(name, "LightToken", "has correct name");
+            return tokenInstance.symbol();
+        }).then(function(symbol){
+            assert.equal(symbol, "LT", "has the correct symbol");
+            return tokenInstance.standard();
+        }).then(function(standard){
+            assert.equal(standard, "LightToken v1.0", "has the correct standard");
+        })
+    })
     it('sets the total supply upon deployment', function(){
         return LightToken.deployed().then(function(instance){
             tokenInstance = instance;
@@ -13,4 +28,13 @@ contract('LightToken', function(accounts){
             assert.equal(adminBalance.toNumber(),1000000, 'it allocates the inital supply to the admin')
         })
     });
+
+    // it('transfers token ownership', function(){
+    //     return LightToken.deployed().then(function(instance){
+    //         tokenInstance = instance;
+    //         return tokenInstance.transfer.call(accounts[1], 999999999999999999);
+    //     }).then(assert.fail).catch(function(error){
+    //         assert(error.message.indexOf('revert') >= 0, 'error msg must contain revert')
+    //     })
+    // })
 });
